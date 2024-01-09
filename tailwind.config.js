9c88cbb4394fb377a8cd00ad571c09df22e66bae
Bob Stanley <dakotasfacts@gmail.com>
2024-01-09 11:12:32 +0000

    Added -image -Custom Fonts     -Google fonts         Poppins         Roboto         Comfortaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'logo': ['Comfortaa'],
+      'create': ['Poppins'],
+      'body': ['Roboto'],
+    },
     extend: {},
   },
   plugins: [],
